cbd13cfc271898917aba6297c0b31834bfe83caa
andz-bb <andrew.thompson@budibase.com>
2022-09-23 11:59:22 +0100

    first commit

---
+++ src/Component.svelte
@@ -0,0 +1,110 @@
+<script>
+  import { getContext, onDestroy } from "svelte";
+  import Icon from "./Icon.svelte";
+
+  export let color;
+  export let numberOfStars;
+  export let field;
+  export let label;
+  export let size = "24px";
+  export let type = "star";
+  export let variant = "mono";
+
+  const { styleable } = getContext("sdk");
+  const component = getContext("component");
+  const formContext = getContext("form");
+  const formStepContext = getContext("form-step");
+  const fieldGroupContext = getContext("field-group");
+
+  let fieldApi;
+  let fieldState;
+  let hoverRating = 0;
+
+  const formApi = formContext?.formApi;
+  const labelPos = fieldGroupContext?.labelPosition || "above";
+  $: formStep = formStepContext ? $formStepContext || 1 : 1;
+  $: formField = formApi?.registerField(
+    field,
+    "number",
+    0,
+    false,
+    null,
+    formStep
+  );
+
+  $: unsubscribe = formField?.subscribe((value) => {
+    fieldState = value?.fieldState;
+    fieldApi = value?.fieldApi;
+  });
+
+  $: labelClass =
+    labelPos === "above" ? "" : `spectrum-FieldLabel--${labelPos}`;
+
+  onDestroy(() => {
+    fieldApi?.deregister();
+    unsubscribe?.();
+  });
+</script>
+
+<div class="spectrum-Form-item" use:styleable={$component.styles}>
+  {#if !formContext}
+    <div class="placeholder">Form components need to be wrapped in a form</div>
+  {:else}
+    <label
+      class:hidden={!label}
+      for={fieldState?.fieldId}
+      class={`spectrum-FieldLabel spectrum-FieldLabel--sizeM spectrum-Form-itemLabel ${labelClass}`}
+    >
+      {label || " "}
+    </label>
+    <div class="spectrum-Form-itemField">
+      {#each { length: numberOfStars } as _, i}
+        <Icon
+          {color}
+          {size}
+          {type}
+          {variant}
+          filled={hoverRating
+            ? hoverRating >= i + 1
+            : fieldState?.value >= i + 1}
+          starId={i + 1}
+          on:mouseover={() => (hoverRating = i + 1)}
+          on:mouseleave={() => (hoverRating = null)}
+          on:click={() => fieldApi.setValue(i + 1)}
+        />
+      {/each}
+
+      {#if fieldState?.error}
+        <div class="error">{fieldState.error}</div>
+      {/if}
+    </div>
+  {/if}
+</div>
+
+<style>
+  .placeholder {
+    color: var(--spectrum-global-color-gray-600);
+  }
+  label {
+    white-space: nowrap;
+  }
+  label.hidden {
+    padding: 0;
+  }
+  .spectrum-Form-itemField {
+    position: relative;
+    width: 100%;
+  }
+  .error {
+    color: var(
+      --spectrum-semantic-negative-color-default,
+      var(--spectrum-global-color-red-500)
+    );
+    font-size: var(--spectrum-global-dimension-font-size-75);
+    margin-top: var(--spectrum-global-dimension-size-75);
+  }
+  .spectrum-FieldLabel--right,
+  .spectrum-FieldLabel--left {
+    padding-right: var(--spectrum-global-dimension-size-200);
+  }
+</style>
---
+++ src/Icon.svelte
@@ -0,0 +1,63 @@
+<script>
+  export let filled = false;
+
+  export let color;
+  export let size;
+  export let type;
+  export let variant;
+
+  $: path =
+    type === "star"
+      ? "m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"
+      : "M44.395 25l-19.395 19.395-19.395-19.395a13.714 13.714 90 0 1 19.395-19.395 13.714 13.714 90 0 1 19.395 19.395z";
+
+  $: className = filled ? variant : "outline";
+</script>
+
+<svg
+  width={size}
+  height={size}
+  viewBox="0 0 50 50"
+  class="icon"
+  on:mouseover
+  on:focus
+  on:mouseleave
+  on:click
+  let:class
+>
+  <path class={className} d={path} style="--rating-custom-color: {color}" />
+</svg>
+
+<style>
+  .icon {
+    margin-right: 2px;
+  }
+  .outline {
+    fill: none;
+    stroke: var(--spectrum-global-color-gray-600);
+  }
+
+  .primary {
+    fill: var(--primaryColor);
+  }
+  .secondary {
+    fill: var(--primaryColorHover);
+  }
+  .mono {
+    fill: var(
+      --spectrum-button-primary-m-background-color-down,
+      var(--spectrum-global-color-gray-900)
+    );
+  }
+  .gold {
+    fill: #ffd700;
+  }
+
+  .red {
+    fill: var(--spectrum-global-color-red-400, red);
+  }
+
+  .custom {
+    fill: var(--rating-custom-color)
+  }
+</style>
